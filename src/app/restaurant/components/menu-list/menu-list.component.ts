9953bb79b28328746d202de5ae092bdaa1620723
kalish93 <kaleabtekalign@gmail.com>
2024-08-24 00:07:29 +0300

    feat: menu list page

--- src/app/restaurant/components/menu-list/menu-list.component.ts
+++ src/app/restaurant/components/menu-list/menu-list.component.ts
@@ -6,5 +6,54 @@ import { Component } from '@angular/core';
   styleUrl: './menu-list.component.scss'
 })
 export class MenuListComponent {
+  menuItems = [
+    {
+      name: 'Spaghetti Carbonara',
+      price: 12.99,
+      description: 'Classic Italian pasta with creamy sauce and pancetta.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.52oQgE8Dako4JWg31n1viAHaKi?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Margherita Pizza',
+      price: 10.99,
+      description: 'Fresh mozzarella, tomatoes, and basil on a thin crust.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.zjwlkbRWoRIUP2nLduDtoAHaLI?pid=ImgDet&w=192&h=288&c=7&dpr=1.5'
+    },
+    {
+      name: 'Margherita Pizza',
+      price: 10.99,
+      description: 'Fresh mozzarella, tomatoes, and basil on a thin crust.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.52oQgE8Dako4JWg31n1viAHaKi?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Margherita Pizza',
+      price: 10.99,
+      description: 'Fresh mozzarella, tomatoes, and basil on a thin crust.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.zjwlkbRWoRIUP2nLduDtoAHaLI?pid=ImgDet&w=192&h=288&c=7&dpr=1.5'
+    },
+    {
+      name: 'Margherita Pizza',
+      price: 10.99,
+      description: 'Fresh mozzarella, tomatoes, and basil on a thin crust.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.52oQgE8Dako4JWg31n1viAHaKi?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Margherita Pizza',
+      price: 10.99,
+      description: 'Fresh mozzarella, tomatoes, and basil on a thin crust.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.52oQgE8Dako4JWg31n1viAHaKi?rs=1&pid=ImgDetMain'
+    },
+    {
+      name: 'Margherita Pizza',
+      price: 10.99,
+      description: 'Fresh mozzarella, tomatoes, and basil on a thin crust.',
+      imageUrl: 'https://th.bing.com/th/id/OIP.52oQgE8Dako4JWg31n1viAHaKi?rs=1&pid=ImgDetMain'
+    },
+    // Add more items as needed
+  ];
 
+  order(item: any): void {
+    // Handle the order logic
+    console.log(`Ordered: ${item.name}`);
+  }
 }
