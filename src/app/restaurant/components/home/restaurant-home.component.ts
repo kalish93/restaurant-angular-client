dcd999277f5d49e50bef5424673c691d67e57842
kalish93 <kaleabtekalign@gmail.com>
2024-08-28 15:53:02 +0300

    feat: role added

--- src/app/restaurant/components/home/restaurant-home.component.ts
+++ src/app/restaurant/components/home/restaurant-home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RxState } from '@rx-angular/state';
+import { AuthFacade } from 'src/app/auth/facade/auth.facade';
 import {
   EMPLOYEES_LIST,
   ROLES_LIST,
@@ -11,38 +13,65 @@ import {
   TABLE_LIST,
 } from 'src/app/core/constants/routes';
 
+import{ jwtDecode} from 'jwt-decode';
+
+interface RestaurantHomeComponentState {
+  accessToken: any;
+}
+
+const initRestaurantHomeComponentState: Partial<RestaurantHomeComponentState> = {
+  accessToken: undefined,
+};
+
 @Component({
   selector: 'app-home',
   templateUrl: './restaurant-home.component.html',
   styleUrls: ['./restaurant-home.component.scss'],
 })
 export class RestaurantHomeComponent {
-  navLinks: Array<{ link: string; label: string; icon: string }> = [
-    {
-      link: RESTAURANT_LIST,
-      label: 'Restaurants',
-      icon: 'restaurant',
-    },
-    {
-      link: TABLE_LIST,
-      label: 'Tables',
-      icon: 'table_chart',
-    },
-    {
-      link: STOCK_LIST,
-      label: 'Stock',
-      icon: 'local_bar',
-    },
-    {
-      link: MENU_LIST,
-      label: 'Menu',
-      icon: 'menu_book',
-    },
-    {
-      link: USERS_LIST,
-      label: 'Users',
-      icon: 'person',
-    },
+  navLinks: Array<{ link: string; label: string; icon: string }> = [];
+
+  $accessToken = this.state.select('accessToken');
+  decoded :any;
+  roleName: any;
+  constructor(
+    private authFacade: AuthFacade,
+    private state: RxState<RestaurantHomeComponentState>
+  ){
+    this.state.set(initRestaurantHomeComponentState);
+    this.state.connect('accessToken', this.authFacade.accessToken$);
+    this.$accessToken.subscribe((token)=>{
+       this.decoded = jwtDecode(token);
+       this.roleName = this.decoded?.role?.name;
+       this.setNavLinks(this.roleName);
+    })
+  }
+  // navLinks: Array<{ link: string; label: string; icon: string }> = [
+  //   {
+  //     link: RESTAURANT_LIST,
+  //     label: 'Restaurants',
+  //     icon: 'restaurant',
+  //   },
+  //   {
+  //     link: TABLE_LIST,
+  //     label: 'Tables',
+  //     icon: 'table_chart',
+  //   },
+  //   {
+  //     link: STOCK_LIST,
+  //     label: 'Stock',
+  //     icon: 'local_bar',
+  //   },
+  //   {
+  //     link: MENU_LIST,
+  //     label: 'Menu',
+  //     icon: 'menu_book',
+  //   },
+  //   {
+  //     link: USERS_LIST,
+  //     label: 'Users',
+  //     icon: 'person',
+  //   },
     // {
     //   link: ROLES_LIST,
     //   label: 'Roles',
@@ -58,5 +87,41 @@ export class RestaurantHomeComponent {
     //   label: 'Admins',
     //   icon: '',
     // },
-  ];
+  // ];
+
+
+  setNavLinks(roleName: string): void {
+    if (roleName === 'Admin') {
+      this.navLinks = [
+        {
+          link: RESTAURANT_LIST,
+          label: 'Restaurants',
+          icon: 'restaurant',
+        },
+        {
+          link: USERS_LIST,
+          label: 'Users',
+          icon: 'person',
+        },
+      ];
+    } else {
+      this.navLinks = [
+        {
+          link: TABLE_LIST,
+          label: 'Tables',
+          icon: 'table_chart',
+        },
+        {
+          link: STOCK_LIST,
+          label: 'Stock',
+          icon: 'local_bar',
+        },
+        {
+          link: MENU_LIST,
+          label: 'Menu',
+          icon: 'menu_book',
+        },
+      ];
+    }
+  }
 }
